9bfc7bc4afc611bf337076b75ccb95290de73cad
Javier Lima <javi.limashelby@gmail.com>
2024-01-19 19:26:05 -0500

    Implement ErrorHandlingMiddleware
    - ErrorHandlingMiddleware deals with all possible exceptions in the project (service and general exceptions)

---
+++ FastBubberDinner.Api/Middleware/ErrorHandlingMiddleware.cs
@@ -0,0 +1,58 @@
+using System.Net;
+using FastBubberDinner.Domain.Common.Errors;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace FastBubberDinner.Api.Middleware;
+
+public class ErrorHandlingMiddleware
+{
+    private readonly RequestDelegate _next;
+    public ErrorHandlingMiddleware(RequestDelegate next)
+    {
+        _next = next;
+    }
+    public async Task Invoke(HttpContext context)
+    {
+        try
+        {
+            await _next(context);
+        }
+        catch (ServiceException exception)
+        {
+            await HandleServiceExceptionAsync(context, exception);
+        }
+        catch (Exception exception)
+        {
+            await HandleNonServiceExceptionAsync(context, exception);
+        }
+    }
+
+    public static Task HandleServiceExceptionAsync(HttpContext context, ServiceException exception)
+    {
+        var result = JsonConvert.SerializeObject(new
+        {
+            Title = exception.Message,
+            exception.Detail,
+        });
+
+        context.Response.ContentType = "application/json";
+        context.Response.StatusCode = exception.Status;
+
+        return context.Response.WriteAsync(result);
+    }
+
+    public static Task HandleNonServiceExceptionAsync(HttpContext context, Exception exception)
+    {
+        var result = JsonConvert.SerializeObject(new
+        {
+            Title = "Internal Server Error",
+            Detail = exception.Message
+        });
+
+        context.Response.ContentType = "application/json";
+        context.Response.StatusCode = 500;
+
+        return context.Response.WriteAsync(result);
+    }
+}
