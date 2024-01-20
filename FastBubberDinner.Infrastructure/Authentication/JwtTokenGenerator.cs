a50a5c6b78e4b23c1f3ea57a3e95036d3ea56fe5
Javier Lima <javi.limashelby@gmail.com>
2024-01-20 10:25:01 -0500

    Fix JwtToken Jti-Claim Bug

--- FastBubberDinner.Infrastructure/Authentication/JwtTokenGenerator.cs
+++ FastBubberDinner.Infrastructure/Authentication/JwtTokenGenerator.cs
@@ -34,7 +34,7 @@ public class JwtTokenGenerator : IJwtTokenGenerator
             new Claim(JwtRegisteredClaimNames.Sub, user.Id.ToString()),
             new Claim(JwtRegisteredClaimNames.GivenName, user.FirstName),
             new Claim(JwtRegisteredClaimNames.FamilyName, user.LastName),
-            new Claim(JwtRegisteredClaimNames.Jti, user.Id.ToString())
+            new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString())
         };
 
         var securityToken = new JwtSecurityToken(
